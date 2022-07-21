b1ba3e68c0edf608fe2d6ad2e8aa0606d8c1fc7b
sohamg01 <80263965+sohamg01@users.noreply.github.com>
2022-07-20 23:40:41 -0400

    Update streamlit_app.py
    adding the cdv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
+import streamlit
+
+ streamlit.title('My Moms New Healthy Dinner')
+
+ streamlit.header('Breakfast Menu')
+ streamlit.text('🍳Omega 3 and Blueberry Oatmeal')
+ streamlit.text('🥬Kale, Spinach and Rocket Smoothe')
+ streamlit.text('🥚Hard boiled Free Range Egg')
+ streamlit.text('🥑Avecado Toast')
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page
 streamlit.dataframe(my_fruit_list)
