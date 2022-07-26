3001001c8ced2f1d82f8d1c602365db750a897a4
sohamg01 <80263965+sohamg01@users.noreply.github.com>
2022-07-26 00:04:00 -0400

    Update streamlit_app.py
    JSON normalized

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())#writes the date to the screen
+
+# Capture Json version of resposne and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
